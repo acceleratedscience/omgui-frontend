2a285952be4e8fb84572e5798582bf00ec2a2229
Moenen Erbuer <moenen@shapish.com>
2024-02-19 09:50:48 +0200

    stash textviewer

--- src/components/RouterViewKA.vue
+++
@@ -1,8 +0,0 @@
-<template>
-	<RouterView v-slot="{ Component }">
-		<KeepAlive>
-			<component v-if="Component" :is="Component" />
-			<div v-else>Loading module...</div>
-		</KeepAlive>
-	</RouterView>
-</template>
--- src/modules/TextViewer.vue
+++ src/modules/TextViewer.vue
@@ -1,7 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// Vue
+import { useRoute, useRouter } from 'vue-router'
+import { computed } from 'vue'
+
+const route = useRoute()
+const router = useRouter()
+
+defineProps({
+	filePath: String,
+})
+
+const path = computed(() => {
+	return route.query.path
+})
+</script>
 
 <template>
-	<main id="module">This is the Text viewer component</main>
+	<main id="module">This is the Text viewer component {{ path }}!</main>
 </template>
 
 <style lang="css" scoped>
