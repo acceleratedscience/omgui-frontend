0be9ba94b7a2d84ae422038633ee63dbcdd5f3b7
Moenen Erbuer <moenen.erbuer@ibm.com>
2025-09-07 14:30:58 +0200

    Removed env file

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
-import 'dotenv/config'
+// import 'dotenv/config'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
